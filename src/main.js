dfc7f149a85538315e826a0f2ff08d4cef18a761
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-12-07 19:17:35 +0100

    Add chuck and trivia store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
 import { router } from './router.js'
+import { chuckStore } from './chuckStore.js'
+import { triviaStore } from './triviaStore.js'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    chuckStore,
+    triviaStore
+  }
+})
+
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
