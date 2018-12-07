6f6796399e43608eb5b5a1bd9531c398df20b4c1
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-12-07 18:47:19 +0100

    Add layout and Trivia app component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <router-link class="navbar-brand" :to="{name: 'trivia-app'}">
+                Trivia app
+            </router-link>
+        </nav>    
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
